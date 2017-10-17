beff9cfd93528f47ebd6faacbf6a2414483aa1b9
Chris <chen1385@gmail.com>
2017-10-17 16:55:20 -0700

    Completed large hero block and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+//for require fileName, no extension .js/.css is required.
+//['styles'] is a dependency; [dependencies] must run and complete before the function will run.
